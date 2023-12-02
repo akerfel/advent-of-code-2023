a743965eef48fae7156158ebca8a25dccf2e6437
Magnus Åkerfeldt <akerfel@kth.se>
2023-12-02 11:27:54 +0100

    finished day1 part 2

--- day1/PartTwo.java
+++ day1/PartTwo.java
@@ -7,7 +7,8 @@ import java.util.List;
 public class PartTwo {
     public static void main(String[] args) throws IOException {
         List<String> lines = new ArrayList<String>();
-        BufferedReader bf = new BufferedReader(new FileReader("Input.txt"));
+        System.out.println(System.getProperty("user.dir"));
+        BufferedReader bf = new BufferedReader(new FileReader("./day1/Input.txt"));
         String newLine = bf.readLine();
        
         while (newLine != null) {
@@ -32,21 +33,68 @@ public class PartTwo {
     }
 
      private static char getFirstDigit(String line) {
-        for (char c : line.toCharArray()) {
-            if (Character.isDigit(c)) {
-                return c;
-            }
+        int n = line.length();
+        for (int i = 0; i < n; i++) {
+            char digit = getDigitAt(line, i);
+            if (digit != '0')
+                return digit;
         }
         return '0';
      }
 
      private static char getSecondDigit(String line) {
-        for (int i = line.length() - 1; i >= 0; i--) {
-            char c = line.charAt(i);
-            if (Character.isDigit(c)) {
-                return c;
-            }
+        int n = line.length();
+        for (int i = n - 1; i >= 0; i--) {
+            char digit = getDigitAt(line, i);
+            if (digit != '0')
+                return digit;
         }
+        return '0';
+     }
+
+     private static char getDigitAt(String line, int i) {
+        int n = line.length();
+
+        // Check for actual digit
+        char c = line.charAt(i);
+        if (Character.isDigit(c)) {
+            return c;
+        }
+
+        // Check for spelled out digit
+        int digitLength = 3;
+        if (i <= n - digitLength) {
+            String substring = line.substring(i, i + digitLength);
+            if (substring.equals("one"))
+                return '1';
+            if (substring.equals("two"))
+                return '2';
+            if (substring.equals("six"))
+                return '6';
+        }
+
+        digitLength = 4;
+        if (i <= n - digitLength) {
+            String substring = line.substring(i, i + digitLength);
+            if (substring.equals("four"))
+                return '4';
+            if (substring.equals("five"))
+                return '5';
+            if (substring.equals("nine"))
+                return '9';
+        }
+
+        digitLength = 5;
+        if (i <= n - digitLength) {
+            String substring = line.substring(i, i + digitLength);
+            if (substring.equals("three"))
+                return '3';
+            if (substring.equals("seven"))
+                return '7';
+            if (substring.equals("eight"))
+                return '8';
+        }
+
         return '0';
      }
 }
